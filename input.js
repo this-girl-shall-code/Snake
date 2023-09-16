a3ad46d1f34c1bff1acc8e3a5aff7810a5a6baf0
this_girl_shall_code <141769052+this-girl-shall-code@users.noreply.github.com>
2023-09-16 10:41:24 +0100

    Update input.js
    added use of WASD keys

--- input.js
+++ input.js
@@ -4,18 +4,26 @@ let lastInputDirection = {x: 0, y:0};
 window.addEventListener('keydown', e =>{
     switch (e.key) {
         case 'ArrowUp':
+        case 'w':
+        case 'W':
             if(lastInputDirection.y != 0 ) break /*if we are already moving up or down, ignore as we can't go down and we're already going up*/
             inputDirection = { x: 0, y: -1 }
             break
         case 'ArrowDown':
+        case 's':
+        case 'S':
             if(lastInputDirection.y != 0 ) break 
             inputDirection = { x: 0, y: 1 }
             break
         case 'ArrowLeft':
+        case 'a':
+        case 'A':
             inputDirection = { x: -1, y: 0 }
             if(lastInputDirection.x != 0 ) break /*if we are already moving left or right, ignore as we can't go right and we're already going left*/
             break
         case 'ArrowRight':
+        case 'd':
+        case 'D':
             if(lastInputDirection.x != 0 ) break
             inputDirection = { x: 1, y: 0 }
             break
